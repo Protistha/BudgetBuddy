3457342f91c067e0de21a9b0531199d13f5e83c5
Protistha Chowdhury <protistha6@gmail.com>
2026-05-03 15:18:17 +0530

    Add imports for machine learning and data visualization

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+import plotly
+import plotly.graph_objs as go
+import json
+import hashlib
+import secrets
+from cryptography.fernet import Fernet
+import requests
